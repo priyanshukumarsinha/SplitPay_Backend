ffc666b15836dbc3311a448a2c65707ba4440101
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-06-22 13:29:01 +0530

    initial Project Setup

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import {router as userRouter} from './routes/user.routes.js'
+import {router as followRouter} from './routes/follow.routes.js'
+
+const app = express();
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+    })
+);
+
+
+// parse application/json
+app.use(express.json({ limit: '16kb' }));
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+
+// Serve static files
+app.use(express.static('public'));
+
+app.use(cookieParser())
+
+app.use('/api', userRouter)
+app.use('/api', followRouter)
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+export { app };
+
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import {app} from './app.js';
+import connectDB from './db/index.js';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server is Listening at Port ${PORT}`);
+})
+
