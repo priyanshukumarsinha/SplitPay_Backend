64b907493d03fe49bd34818a1d659a81c2be7aa9
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-07-18 21:30:31 +0530

    refactor: Update import in utils/index.js to fix typo

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 import { ApiError } from "./ApiError.js";
 import { ApiResponse } from "./ApiResponse.js";
-import { AsyncHandler } from "./AsyncHandler.js";
+import { AsyncHandler } from "./asyncHandler.js";
 import { generateAccessToken } from "./generateAccessToken.js";
 import { generateRefreshToken } from "./generateRefreshToken.js";
 
